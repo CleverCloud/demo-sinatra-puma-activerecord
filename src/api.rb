89bbb9b8a6788e40fc564571a6db5e67aeadbabf
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-10-03 12:31:07 +0200

    add api
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ src/api.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'sinatra/activerecord'
+require 'sinatra/base'
+require 'sinatra/json'
+require 'sinatra/reloader'
+require 'uuid'
+
+require_relative '../models/user'
+
+class DemoApi < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/:name' do
+    user = User.find_by(name: params[:name])
+    halt 404 if user.nil?
+    json user
+  end
+
+  post '/' do
+    request.body.rewind
+    name = request.body.read.strip
+    halt 400 if name.empty?
+    user = User.create(user_id: UUID.generate, name: name)
+    halt 400 unless user.valid?
+    json user
+  end
+end
